b21e74d6fa79b5fce02c0747fd96ac5b60b64a6c
lbfn83 <lbfn83@gmail.com>
2020-07-19 23:08:02 -0400

    new change confusing :q

--- lists/tests.py
+++ lists/tests.py
@@ -9,7 +9,12 @@ from lists.views import home_page
 # Create your tests here.
 class HomePageTest(TestCase):
     def test_uses_home_template(self):
+        #urls.py와 views.py가 제대로 잘 동작하는지 정도만 확인하는 거지
         response = self.client.get('/')
         self.assertTemplateUsed(response,'home.html')
 
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text':'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
         
