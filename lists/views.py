5c857b7b687e11140295c04454efedd5e606222c
lbfn83 <lbfn83@gmail.com>
2020-08-04 15:03:14 -0400

    new URL + view for adding to existing lists.FT passes :)

--- lists/views.py
+++ lists/views.py
@@ -5,19 +5,19 @@ from lists.models import Item, List
 # Create your views here.
 #home_page = None
 
+def add_item(request, list_id):
+    list_ = List.objects.get(id = list_id)
+    Item.objects.create(text=request.POST['item_text'], list = list_)
+    return redirect(f'/lists/{list_.id}/')
+
 def new_list(request):
     list_ = List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/the-only-list-in-the-world/')
+    return redirect(f'/lists/{list_.id}/')
 
-def view_list(request):
-    items = Item.objects.all()
-    print("*****view_view_list**********")
-    for a in items:
-        print(a.text)
-
-    print("*****view_view_list**********")
-    return render(request, 'list.html', {'items':items})
+def view_list(request, list_id):
+    list_ = List.objects.get(id = list_id)
+    return render(request, 'list.html', {'list':list_})
 
 
 def home_page(request):
