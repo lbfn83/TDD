a04cbc31d2222fc1d340fcd8370701538ab322b3
lbfn83 <lbfn83@gmail.com>
2020-08-04 22:17:58 -0400

    End of chap7

---
+++ lists/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+
+from lists import views
+
+urlpatterns = [
+    # Examples:
+    url(r'^(\d+)/$', views.view_list,name='view_list'),
+    url(r'^new$', views.new_list, name='new_list'),
+    url(r'^(\d+)/add_item$', views.add_item,name='add_item'),
+    #url(r'^blog/', include('blog.urls')),
+
+    #url(r'^admin/', include(admin.site.urls)),
+]
