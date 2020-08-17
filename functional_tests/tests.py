fc17180527f6fa1c490d33894886e14773503650
lbfn83 <lbfn83@gmail.com>
2020-08-16 21:38:07 -0400

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,4 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
@@ -6,7 +6,7 @@ import time
 
 MAX_WAIT = 10
 
-class NewVisitorTest(LiveServerTestCase): #
+class NewVisitorTest(StaticLiveServerTestCase): #
 
     def setUp(self): #
         self.browser = webdriver.Firefox()
@@ -21,6 +21,8 @@ class NewVisitorTest(LiveServerTestCase): #
         while True:
             try:
                 table = self.browser.find_element_by_id('id_list_table')
+                print(self.browser)
+
                 #table = self.browser.find_element_by_id('id_nothing')
                 rows = table.find_elements_by_tag_name('tr')
                 #self.assertIn('foo', [row.text for row in rows])
@@ -128,4 +130,28 @@ class NewVisitorTest(LiveServerTestCase): #
 
         #satisfied, they both go back to sleep
 
+    def test_layout_and_styling(self):
+        #Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024,768)
 
+        #she notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+                inputbox.location['x'] + inputbox.size['width']/2,
+                512,
+                delta=10
+                )
+        #She starts a new list and sees the input is nicely
+        # centered there too
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1:testing')
+        inputbox = self.browser.find_element_by_id('id_new_item') 
+        self.assertAlmostEqual(
+                inputbox.location['x'] + inputbox.size['width']/2,
+                512,
+                delta=10
+                )
+
+        
